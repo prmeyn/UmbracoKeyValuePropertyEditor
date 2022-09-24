1255888480985dbaf1c352e1b962e0dd9dbdfab0
prmeyn <100074068+prmeyn@users.noreply.github.com>
2022-09-24 17:28:40 +0200

    SingleKeyValuePickerConvertor

--- UmbracoKeyValuePropertyEditor/SingleKeyValuePickerConvertor.cs
+++ UmbracoKeyValuePropertyEditor/SingleKeyValuePickerConvertor.cs
@@ -1,8 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.PropertyEditors;
 
@@ -10,36 +5,20 @@ namespace UmbracoKeyValuePropertyEditor
 {
 	public class SingleKeyValuePickerConvertor : IPropertyValueConverter
 	{
-		public object ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
-		{
-			throw new NotImplementedException();
-		}
+		public bool IsConverter(IPublishedPropertyType propertyType) => propertyType.EditorAlias == "SingleKeyValuePicker";
+		public object ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview) => inter as string;
 
 		public object ConvertIntermediateToXPath(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
 		{
 			throw new NotImplementedException();
 		}
 
-		public object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview)
-		{
-			throw new NotImplementedException();
-		}
+		public object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview) => source as string;
 
-		public PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType)
-		{
-			throw new NotImplementedException();
-		}
+		public PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType) => PropertyCacheLevel.Element;
 
-		public Type GetPropertyValueType(IPublishedPropertyType propertyType)
-		{
-			throw new NotImplementedException();
-		}
+		public Type GetPropertyValueType(IPublishedPropertyType propertyType) => typeof(string);
 
-		public bool IsConverter(IPublishedPropertyType propertyType) => propertyType.EditorAlias == "SingleKeyValuePicker";
-
-		public bool? IsValue(object value, PropertyValueLevel level)
-		{
-			throw new NotImplementedException();
-		}
+		public bool? IsValue(object value, PropertyValueLevel level) => string.IsNullOrWhiteSpace(value as string);
 	}
 }
