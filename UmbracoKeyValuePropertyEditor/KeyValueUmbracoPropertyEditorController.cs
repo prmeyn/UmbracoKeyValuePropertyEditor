bec868b740858e1c8db5450acbb6d3ad5e2a473c
Antony Meyn <anm@umbraco.dk>
2024-09-24 20:49:34 +0000

    bool instead of int

--- UmbracoKeyValuePropertyEditor/KeyValueUmbracoPropertyEditorController.cs
+++ UmbracoKeyValuePropertyEditor/KeyValueUmbracoPropertyEditorController.cs
@@ -6,6 +6,6 @@ namespace UmbracoKeyValuePropertyEditor
 {
 	public abstract class KeyValueUmbracoPropertyEditorController : ManagementApiControllerBase
 	{
-		public abstract IOrderedEnumerable<KeyValuePair<string, string>> GetKeyValueList(string parentNodeIdOrGuid, string nodeIdOrGuid, string propertyAlias, int uniqueFilter, int allowNull);
+		public abstract IOrderedEnumerable<KeyValuePair<string, string>> GetKeyValueList(string parentNodeIdOrGuid, string nodeIdOrGuid, string propertyAlias, bool uniqueFilter, bool allowNull);
 	}
 }
